73d9838fc439b9ff447f1658c7738b5c1fc91bef
yana <yanayuniarsyah@gmail.com>
2026-09-23 07:08:02 +0700

    Hide scrollbar in print via overflow:hidden

--- api.php
+++ api.php
@@ -3,7 +3,7 @@
 
 // CORS and response headers
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, x-user-role");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, x-user-role, x-wlc-token");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Content-Type: application/json; charset=UTF-8");
 
